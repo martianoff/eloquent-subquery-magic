a9714cb571015043823bc07ea3ae4f19fec8b2e8
Maksim Martianov <maxmru@yandex.ru>
2019-11-01 11:27:33 -0400

    adjust dev dependencies

--- phpinsights.php
+++ phpinsights.php
@@ -1,12 +1,18 @@
 <?php
+
 declare(strict_types=1);
 
-use NunoMaduro\PhpInsights\Domain\Insights\ForbiddenDefineGlobalConstants;
-use NunoMaduro\PhpInsights\Domain\Sniffs\ForbiddenSetterSniff;
+use NunoMaduro\PhpInsights\Domain\Insights\ForbiddenDefineFunctions;
+use NunoMaduro\PhpInsights\Domain\Insights\ForbiddenFinalClasses;
+use NunoMaduro\PhpInsights\Domain\Insights\ForbiddenNormalClasses;
+use NunoMaduro\PhpInsights\Domain\Insights\ForbiddenPrivateMethods;
+use NunoMaduro\PhpInsights\Domain\Insights\ForbiddenTraits;
+use NunoMaduro\PhpInsights\Domain\Metrics\Architecture\Classes;
 use PHP_CodeSniffer\Standards\Generic\Sniffs\Files\LineLengthSniff;
-use PHP_CodeSniffer\Standards\Generic\Sniffs\PHP\NoSilencedErrorsSniff;
-use SlevomatCodingStandard\Sniffs\Functions\UnusedParameterSniff;
+use SlevomatCodingStandard\Sniffs\Namespaces\AlphabeticallySortedUsesSniff;
+use SlevomatCodingStandard\Sniffs\TypeHints\DeclareStrictTypesSniff;
 use SlevomatCodingStandard\Sniffs\TypeHints\DisallowMixedTypeHintSniff;
+use SlevomatCodingStandard\Sniffs\TypeHints\TypeHintDeclarationSniff;
 
 return [
     /*
@@ -35,11 +41,18 @@ return [
     'exclude' => [
     ],
     'add' => [
-        \NunoMaduro\PhpInsights\Domain\Metrics\Code\Comments::class => [
-            \PhpCsFixer\Fixer\Phpdoc\PhpdocSummaryFixer::class,
+        Classes::class => [
+            ForbiddenFinalClasses::class,
         ],
     ],
     'remove' => [
+        AlphabeticallySortedUsesSniff::class,
+        DeclareStrictTypesSniff::class,
+        DisallowMixedTypeHintSniff::class,
+        ForbiddenDefineFunctions::class,
+        ForbiddenNormalClasses::class,
+        ForbiddenTraits::class,
+        TypeHintDeclarationSniff::class,
     ],
     'config' => [
         LineLengthSniff::class => [
@@ -47,34 +60,8 @@ return [
             'absoluteLineLimit' => 120,
             'ignoreComments' => true,
         ],
-        DisallowMixedTypeHintSniff::class => [
-            'exclude' => [
-                'src/Domain/Reflection.php',
-                'src/Domain/Details.php',
-            ],
-        ],
-        ForbiddenSetterSniff::class => [
-            'exclude' => [
-                'src/Domain/Reflection.php',
-                'src/Domain/Details.php',
-            ],
-        ],
-        NoSilencedErrorsSniff::class => [
-            'exclude' => [
-                'src/Domain/Analyser.php',
-                'src/Domain/File.php',
-            ],
-        ],
-        ForbiddenDefineGlobalConstants::class => [
-            'ignore' => [
-                'PHP_CODESNIFFER_VERBOSITY',
-                'PHP_CODESNIFFER_CBF',
-            ],
-        ],
-        UnusedParameterSniff::class => [
-            'exclude' => [
-                'src/Domain/LinkFormatter/NullFileLinkFormatter.php',
-            ],
+        ForbiddenPrivateMethods::class => [
+            'title' => 'The usage of private methods is not idiomatic in Laravel.',
         ],
     ],
 ];
